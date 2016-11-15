731968eeab6c7ee18b46cc999cbd350e3234f45b
VitorCapuano <capuano.vitor@gmail.com>
2016-11-14 22:57:09 -0200

    Mudança do settings.py

--- DjangoMooca/settings.py
+++ DjangoMooca/settings.py
@@ -125,17 +125,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
-#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#DEFAULT_FROM_EMAIL = 'admin@djangoeccomerce.com.br'
-#EMAIL_USE_TLS = True
-#EMAIL_HOST = 'smtp.gmail.com'
-#EMAIL_HOST_USER = 'testeappscapu@gmail.com'
-#EMAIL_HOST_PASSWORD = ''
-#EMAIL_PORT = 587
-
-#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 #Email
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 DEFAULT_FROM_EMAIL = 'admin@djangoecommerce.com'
